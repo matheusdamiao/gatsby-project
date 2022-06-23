31806ef5a0da86da021fd87461d27d15373f8bd1
Matheus Damião <matheus.damiaoliveira@gmail.com>
2022-06-23 19:39:03 -0300

    trying some plugins

--- src/pages/about.js
+++ src/pages/about.js
@@ -1,12 +1,18 @@
 
 import * as React from 'react'
 import Layout from '../components/layout'
+import { StaticImage } from 'gatsby-plugin-image'
 
 
 const AboutPage = () => {
   return (
     <Layout pageTitle='About'>
       <p> This is my Bio</p>
+      <StaticImage
+      width={200}
+      src="../images/photo.jpeg"
+      alt="personal photo"
+      />
     </Layout>
   )
 }
--- src/pages/index.js
+++ src/pages/index.js
@@ -1,5 +1,6 @@
 
 
+import { StaticImage } from 'gatsby-plugin-image'
 import * as React from 'react'
 import Layout from '../components/layout'
 
@@ -9,6 +10,12 @@ const IndexPage = () => {
     <>
     <Layout pageTitle="Home Page">
       <p> Let's test this page component</p>
+      <StaticImage
+      width={350}
+      src="https://i.pinimg.com/originals/63/da/43/63da438514e0b4990863dacc3f8ab4c7.jpg"
+      alt="a photo of a pittbull puppy"
+      />
+
     </Layout>
     
     

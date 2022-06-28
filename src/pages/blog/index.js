cde2ba180c1be676b3c950c9649557726aedbcb0
Matheus Damião <matheus.damiaoliveira@gmail.com>
2022-06-28 11:18:03 -0300

    adding slug pages to blog

--- src/pages/blog/index.js
+++ src/pages/blog/index.js
@@ -1,18 +1,20 @@
 import React from 'react'
-import Layout from '../components/layout'
-import { graphql } from 'gatsby'
-import { MDXRenderer } from 'gatsby-plugin-mdx'
+import Layout from '../../components/layout'
+import { graphql, Link } from 'gatsby'
+
 
 const Blog = ({data}) => {
   return (
     <Layout pageTitle="Blog">
         {data.allMdx.nodes.map( (node) =>(
           <article key={node.id}>
-            <h2>{node.frontmatter.title}</h2> 
+            <h2>
+              <Link to={`/blog/${node.slug}`}>
+              {node.frontmatter.title}
+              </Link>
+            </h2>
             <p> Posted: {node.frontmatter.date}</p>
-            <MDXRenderer>
-             {node.body}
-            </MDXRenderer>
+          
           </article>
            )      
           )}
@@ -32,7 +34,7 @@ export const query = graphql`
           title
         }
         id
-        body
+        slug
       }
     }
   }
